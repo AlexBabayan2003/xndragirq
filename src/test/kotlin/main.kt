638be1dcabd96f615f8f5b3305fd2170439de57a
AlexBabayan2003 <121566333+AlexBabayan2003@users.noreply.github.com>
2023-05-23 06:22:02 -0700

    Update main.kt
    151-172

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,11 +1,17 @@
 import kotlin.math.sqrt
 import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.math.pow
+import kotlin.math.cbrt
 
 fun main() {
 
-    val numbers = List(10) { Random.nextInt(1, 300) }
-    println("random numbers $numbers")
+//    val numbers = List(10) { Random.nextInt(1, 999) }
+//    println("random numbers $numbers")
     val values = mutableListOf<Int>()
+    
+}
+
 
 //
 //151
@@ -122,7 +128,7 @@ fun main() {
 //    numbers.forEach { if (it in 100..999 && it % 3 == 1 && it % 4 == 2) values.add(it) }
 //    if (values.isNotEmpty()) {
 //        println("three-digit numbers that divided by 3 with 1 remained and by 4 remained with 2 values: $values")
-//    } else println("theres no three-digit numbers that divided by 3 with 1 remained and by 4 remained with 2")
+//    } else println("there`s no three-digit numbers that divided by 3 with 1 remained and by 4 remained with 2")
 //
 //    val valuesProducts = mutableListOf<Int>()
 //    for (i in values.indices) {
@@ -132,5 +138,154 @@ fun main() {
 //    if (values.size > 1) println("the product of numbers is $valuesProducts")
 //    else false
 
-}
+//160 - 163 :? = null
 
+//    numbers.forEach { if (it in 100..999) values.add(it) }
+//    val sorted = values.sorted()
+//    println("three-digit sorted numbers $sorted")
+//    var foundNumber: Int? = null
+//
+//    for (i in sorted.indices){
+//        if ((sorted[i] * 16) == (sqrt(sorted[i].toDouble()).toInt())){
+//            foundNumber = sorted[i]}
+//            break
+//    }
+//    if (foundNumber != null) println(foundNumber)
+//    else println("didn`t found the number to product by 16 and equals square of natural number")
+
+//164: exception on (sqrt(sorted[i].toDouble()).toInt() > n)
+
+//val n = {Random.nextInt(1,200)}
+//numbers.forEach { if (it in 100..999) values.add(it) }
+//val sorted = values.sorted()
+//for (i in sorted.indices){
+//    if (sqrt(sorted[i].toDouble()).toInt() > n) println("the square root of ${sorted[i]} is higher than $n")
+//}
+
+//165
+
+//fun Int.cube(): Int {
+//    return this * this * this
+//}
+//val n = Random.nextInt(1,300)
+//var f = false
+//if (n == n.cube()) f = true
+
+//166
+//
+//    var y = 0
+//
+//    val n = Random.nextInt(1,300)
+//    println(n)
+//
+//    val result = sqrt(sqrt(n.toDouble()))
+//
+//    if (result % 1.0 == 0.0) {
+//        val naturalResult = result.toInt()
+//        y =1
+//        println("√√$n equals $naturalResult: y - $y")
+//    } else {
+//        println("√√$n isn`t a natural number: y - $y")
+//    }
+
+//167
+
+//    var y = false
+//    val x = 1
+//    for (x in 2 until 30) {
+//        if (x < 0) y = true
+//    }
+//    println(message = y)
+//}
+
+//168**
+
+//    fun isPrime(n: Int): Boolean {
+//        if (n <= 1) return false
+//        for (i in 2 until n) {
+//            if (n % i == 0) return false
+//        }
+//        return true
+//    }
+//
+//    val n = Random.nextInt(2, 300)
+//    println(n)
+//    var p = false
+//    val primeNum = isPrime(n)
+//    if (primeNum) {
+//        p = true
+//        println("is $n prime number?: $p")
+//    }
+//    else {
+//        p = false
+//        println("is $n prime number?: $p")
+//    }
+
+//169
+
+//    fun isPrime(n: Int): Boolean {
+//        if (n <= 1) return false
+//        for (i in 2 until n) {
+//            if (n % i == 0) return false
+//        }
+//        return true
+//    }
+//
+//    val x = Random.nextInt(1, 300)
+//    val y = Random.nextInt(1, 300)
+//    var z = 5
+//    val primeNum = isPrime(x + y)
+//    if (primeNum) println(z)
+//    else {
+//        z = 6
+//        println(z)
+//    }
+
+// 170 *grelem gpt i ognutyamb
+
+//    val n = Random.nextInt(1, 300)
+//    println("n = $n")
+//
+//    var number = n + 1
+//    val squareRoot = sqrt(number.toDouble())
+//    var smallestNumber: Int? = null
+//
+//    while (smallestNumber == null) {
+//        if (squareRoot % 1 == 0.0) {
+//            smallestNumber = number
+//        }
+//        number++
+//    }
+//
+//    println("smallest number that higher than $n and being a square of natural number is $smallestNumber")
+
+//171
+
+//val n = Random.nextInt(3, 11)
+//println(n)
+//var factorial: Int = 1
+//
+//for (i in 1..n) {
+//    factorial *= i
+//}
+//
+//println("$n's factorial: $factorial")
+
+////172
+
+//val n = Random.nextInt(4, 12)
+//var eFactorial = 1
+//var oFactorial = 1
+//if (n % 2 == 0){
+//    for (i in 2..n step 2){
+//        eFactorial *= i
+//    }
+//    println("double factorial of $n is $eFactorial")
+//}else {
+//    for (i in 1..n step 2){
+//        oFactorial *= i
+//    }
+//    println("double factorial of $n is $oFactorial")
+//}
+
+//173 - 179?
